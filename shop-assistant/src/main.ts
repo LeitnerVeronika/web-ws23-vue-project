f8cf591b2e422c9513b02cd5ca5d4880ec8018d1
Laura <laura.haberl@gmail.com>
2024-01-02 16:50:11 +0100

    installed FontAwesome and made a global font-awesome-icon component; deleted all icon component files expect market component file

--- shop-assistant/src/main.ts
+++ shop-assistant/src/main.ts
@@ -6,9 +6,19 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+library.add(fas, far);
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// global FontAwesome-Icon component
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app')
--- shop-assistant/src/views/HomeView.vue
+++ shop-assistant/src/views/HomeView.vue
@@ -7,3 +7,19 @@ import Navigation from "@/components/Navigation.vue";
   <main>
   </main>
 </template>
+
+<!--
+  Icons:
+
+  <font-awesome-icon :icon="['fas', 'cart-plus']" />
+
+  filled star = fas; unfilled star = far
+  <font-awesome-icon :icon="['fas', 'star']" />
+  <font-awesome-icon :icon="['far', 'star']" />
+
+  <font-awesome-icon :icon="['fas', 'angle-down']" />
+  <font-awesome-icon :icon="['fas', 'angle-up']" />
+  <font-awesome-icon :icon="['fas', 'bell']" />
+  <font-awesome-icon :icon="['fas', 'times']" />
+
+-->
