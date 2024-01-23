28854fd118706f6dbaae1e56fca46134bf199ea4
Larissa Schäfer <larito@gmx.de>
2024-01-23 18:20:33 +0100

    responsive search, filter row for Product Search Page

--- shop-assistant/src/views/SearchView.vue
+++ shop-assistant/src/views/SearchView.vue
@@ -1,79 +1,77 @@
 <script setup lang="ts">
-  import ProductTypes from "@/components/enums/ProductTypes";
-  const markets = "spar,billa"
-  import axios from "axios";
-  import {onMounted, ref} from "vue";
-  import ProductContainer from "@/components/ProductContainer.vue";
-  import SearchBar from "@/components/SearchBar.vue";
-  import {useRoute} from "vue-router";
-  import Filter from '@/components/Filter.vue';
+import ProductTypes from "@/components/enums/ProductTypes";
+const markets = "spar,billa"
+import axios from "axios";
+import { onMounted, ref } from "vue";
+import ProductContainer from "@/components/ProductContainer.vue";
+import SearchBar from "@/components/SearchBar.vue";
+import { useRoute } from "vue-router";
+import Filter from '@/components/Filter.vue';
 
-  const data = ref(null);
-  const filteredProducts = ref([]);
-  const loading = ref(false);
-  const error = ref(null);
-  let products = ref(null);
-  let searchString = ref('semmel');
+const data = ref(null);
+const filteredProducts = ref([]);
+const loading = ref(false);
+const error = ref(null);
+let products = ref(null);
+let searchString = ref('semmel');
 
-  const route = useRoute();
-  searchString = route.query.query;
+const route = useRoute();
+searchString = route.query.query;
 
 
 
-  onMounted(async () => {
-    loading.value = true;
-    try {
-      const response = await axios.get('http://localhost:3000/api/search?productName=' + searchString +'&markets=' + markets);
-      data.value = response.data;
-    } catch (err) {
-      error.value = 'Error fetching data';
-    } finally {
-      loading.value = false;
-      products = data.value.products;
-    }
-    filteredProducts.value = products.value;
-    console.log(filteredProducts);
-  });
-
-  const searchForProducts = (search) => {
-    searchString = search;
-    performSearch()
+onMounted(async () => {
+  loading.value = true;
+  try {
+    const response = await axios.get('http://localhost:3000/api/search?productName=' + searchString + '&markets=' + markets);
+    data.value = response.data;
+  } catch (err) {
+    error.value = 'Error fetching data';
+  } finally {
+    loading.value = false;
+    products = data.value.products;
   }
+  filteredProducts.value = products.value;
+  console.log(filteredProducts);
+});
 
-  const performSearch = (async () => {
-    loading.value = true;
-    try {
-      const response = await axios.get('http://localhost:3000/api/search?productName=' + searchString +'&markets=' + markets);
-      data.value = response.data;
-    } catch (err) {
-      error.value = 'Error fetching data';
-    } finally {
-      loading.value = false;
-      products = data.value.products;
-    }
-    filteredProducts.value = products.value;
-    console.log(filteredProducts);
-  });
+const searchForProducts = (search) => {
+  searchString = search;
+  performSearch()
+}
 
-  function handleFilteredProducts(filtered) {
-    filteredProducts.value = filtered;
+const performSearch = (async () => {
+  loading.value = true;
+  try {
+    const response = await axios.get('http://localhost:3000/api/search?productName=' + searchString + '&markets=' + markets);
+    data.value = response.data;
+  } catch (err) {
+    error.value = 'Error fetching data';
+  } finally {
+    loading.value = false;
+    products = data.value.products;
   }
+  filteredProducts.value = products.value;
+  console.log(filteredProducts);
+});
+
+function handleFilteredProducts(filtered) {
+  filteredProducts.value = filtered;
+}
 
 </script>
 
 <template>
-  <SearchBar @search="searchForProducts"/>
-  <Filter :originalProducts="products" :type="ProductTypes.search" @filtered="handleFilteredProducts" />
+  <div class="flex-container">
+    <SearchBar @search="searchForProducts" />
+    <Filter :originalProducts="products" :type="ProductTypes.search" @filtered="handleFilteredProducts" />
+  </div>
   <main>
     <div v-if="loading">Loading...</div>
     <div v-else-if="error">{{ error }}</div>
     <div v-else>
-      <ProductContainer v-if="!filteredProducts"
-                        :type="ProductTypes.search"
-                        :data="{ products }" />
-      <ProductContainer v-if="filteredProducts"
-                        :type="ProductTypes.search"
-                        :data="{ products: filteredProducts }" />
+      <ProductContainer v-if="!filteredProducts" :type="ProductTypes.search" :data="{ products }" />
+      <ProductContainer v-if="filteredProducts" :type="ProductTypes.search" :data="{ products: filteredProducts }" />
 
       <div v-else>Keine Produkte gefunden.</div>
     </div>
@@ -81,5 +79,17 @@
 </template>
 
 <style scoped>
+.flex-container {
+  display: flex;
+  flex-direction: column;
+  margin: 0.5rem 0;
+}
 
+@media (min-width: 460px) {
+  .flex-container {
+    flex-direction: row;
+    align-items: center;
+    margin: 0.5rem 1rem;
+  }
+}
 </style>
