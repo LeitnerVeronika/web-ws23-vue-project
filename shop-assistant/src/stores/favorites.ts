0a0bc8d34581b32be4ee849729bf385a7622f07f
LeitnerVeronika <s2010238045@students.fh-hagenberg.at>
2024-01-30 15:07:24 +0100

    Fix: Add logs for debuging to store

--- shop-assistant/src/stores/favorites.ts
+++ shop-assistant/src/stores/favorites.ts
@@ -1,27 +1,34 @@
-import { reactive} from 'vue'
-import { defineStore } from 'pinia'
+import { reactive } from "vue";
+import { defineStore } from "pinia";
 
 /** this store is used
  *  by the ProductContainer component to add/remove a product into the localStorage and
  *  to show the products in the favoritesView */
 export const useFavoriteStore = defineStore('favorites ', () => {
-  const products: Product[] = reactive([]);
+  const products: Product[] = reactive([])
 
   function addProduct(data: Product) {
     products.push(data)
-    localStorage.setItem('favorites', JSON.stringify(products));
-  }
-  function removeProduct(data: Product){
-    products.forEach( (item, index) => {
-      if(products.some((item) => item.name === data.name)){
-        console.log("Remove Item: " + item.name)
-        console.log("Remove Data: " + data.name)
-        products.splice(index,1);
-        console.log("Item Sting: " + products);
-      }
-    });
-    localStorage.setItem('favorites', JSON.stringify(products));
+    localStorage.setItem('favorites', JSON.stringify(products))
   }
 
-  return { products, addProduct, removeProduct}
+  function removeProduct(data: Product) {
+    products.forEach((item, index) => {
+      if (item.name == data.name) {
+        console.log('Remove Item: ' + item.name)
+        console.log('Remove Data: ' + data.name)
+        products.splice(index, 1)
+        console.log('Item Sting: ' + products)
+      }
+      // if(products.some((item) => item.name === data.name)){
+      //   console.log("Remove Item: " + item.name)
+      //   console.log("Remove Data: " + data.name)
+      //   products.splice(index,1);
+      //   console.log("Item Sting: " + products);
+      // }
+    })
+    localStorage.setItem('favorites', JSON.stringify(products))
+  }
+
+  return { products, addProduct, removeProduct }
 })
