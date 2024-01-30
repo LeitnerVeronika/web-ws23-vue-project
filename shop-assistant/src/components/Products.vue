87017e29ec6023ac6d7dce815176882273bfc2ac
LeitnerVeronika <s2010238045@students.fh-hagenberg.at>
2024-01-30 15:50:41 +0100

    Fix: Update Checkbox for sort

--- shop-assistant/src/components/Products.vue
+++ shop-assistant/src/components/Products.vue
@@ -44,7 +44,7 @@ const props = defineProps({
 
 const emit = defineEmits()
 const favStore = useFavoriteStore()
-// let isCheckedComp = ref(props.isChecked)
+let isCheckedComp = ref(props.isChecked)
 
 function addToFavorites() {
   favStore.addProduct(props)
@@ -69,13 +69,22 @@ const removeFromCart = () => {
 }
 
 /** watch the checkbox and triggers a custom event named 'checked' to update the product props inside the ProductContainer */
-watch(props.isChecked, () => {
-  emit('checked', props.name, props.isChecked)
+watch(isCheckedComp, () => {
+  emit('checked', props.name, isCheckedComp.value)
 })
+
+watch(props.isChecked, () =>{
+  if(props.isChecked){
+    isCheckedComp.value = true;
+  }else {
+    isCheckedComp.value = false;
+  }
+})
+
 </script>
 <template>
   <section class="grid-container" :class="{ crossed: isChecked }">
-    <input v-if="type == 2" type="checkbox" v-model="isChecked" />
+    <input v-if="type == 2" type="checkbox" v-model="isCheckedComp" />
     <div v-else></div>
     <router-link
       class="product-link"
